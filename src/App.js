9246eed072debe57da807a3ef94e976168d45e64
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2023-09-05 20:59:11 +0300

    I'm trying to do it by myself

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-
-
 function App() {
   return (
-   <>
-
-    instagram-clone
-   
-  </>
+   <div className="h-full w-full flex items-center justify-center pt-40">
+     <div className="w-[380px] h-[581.15px] bg-hero-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px] ">
+        <img className="w-[250px] h-[538.83px]" url='https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www' alt="" />
+     </div>
+  </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk)",
+        'ss-pattern': "url(https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot2.png?__d=www)",
+    },
   },
+},
   plugins: [],
-}
 
+}
