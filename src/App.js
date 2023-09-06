c99dfa6b5aaae05e8f539888ab9f008164f705f6
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2023-09-06 20:26:04 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 function App() {
   return (
-   <div className="h-full w-full flex items-center justify-center pt-40">
+   <div className="h-full w-full flex items-center justify-center " >
      <div className="w-[380px] h-[581.15px] bg-hero-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px] ">
-        <img className="w-[250px] h-[538.83px]" url='https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www' alt="" />
+       <div className="w-[250px] h-[538.83px] bg-ss-pattern " >
+             
+        </div>
      </div>
   </div>
   );
