b06c7ec34dc70cb28c1410323f38f22c7266746d
Mehmet Fatih Nazıroğlu <m.fatihnaziroglu@gmail.com>
2023-09-08 18:18:10 +0300

    08.09.23

--- src/App.js
+++ src/App.js
@@ -1,12 +1,83 @@
+import React, { useEffect, useRef, useState } from "react";
+import Input from "components/input";
+import {AiFillFacebook} from "react-icons/ai";
+
 function App() {
+  const ref = useRef();
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const enable = username && password
+
+  useEffect(() => {
+    const loadImages = () => {
+      let images = ref.current.querySelectorAll('img');
+      let total = images.length;
+      let current = 0;
+      const imageSlider = () => {
+        if (current > 0) {
+          images[current - 1].classList.add('opacity-0');
+        } else {
+          images[total - 1].classList.add('opacity-0')
+        }
+        images[current].classList.remove('opacity-0');
+        if (current === total - 1) {
+          current = 0;
+        } else {
+          current += 1;
+        }
+      }
+      imageSlider()
+      let interval  = setInterval(imageSlider, 3000);
+
+      return () => {
+       
+        clearInterval(interval);
+      };
+    };
+
+    if (ref.current) {
+      loadImages();
+    }
+  }, [ref]);
+
   return (
-   <div className="h-full w-full flex items-center justify-center " >
-     <div className="w-[380px] h-[581.15px] bg-hero-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px] ">
-       <div className="w-[250px] h-[538.83px] bg-ss-pattern " >
-             
-        </div>
-     </div>
-  </div>
+    <div className="h-full w-full flex items-center justify-center gap-x-8">
+      
+      <div className="w-[380px] h-[581.15px] relative bg-hero-pattern bg-[length:468.32px_634.15px] bg-[top_left_-46px]">
+        <div className="w-[250px] h-[538px] absolute top-[27px] right-[18px]" ref={ref}>
+          
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot1-2x.png/cfd999368de3.png" alt="" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot2-2x.png/80b8aebdea57.png" alt="" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot3-2x.png/fe2540684ab2.png" alt="" />
+          <img className="w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-1000 ease-linear" src="https://www.instagram.com/static/images/homepage/screenshots/screenshot4-2x.png/8e9224a71939.png" alt="" />10        </div>
+      
+      
+      </div>
+      
+      <div className="w-[350px] bg-white border p-[40px] pt-10 pb-2">
+            <a href="#" className=" flex justify-center mb-8 ">
+            <img className="h-[51px]" src="https://www.instagram.com/static/images/web/logged_out_wordmark-2x.png/d2529dbef8ed.png" alt=""/>
+            </a>
+           <form className="grid gap-y-1.5"> 
+           <Input type="text" value={username} onChange={e => setUsername(e.target.value)} label="Phone Number, Username or E-mail" />
+           <Input type="password" value={password} onChange={e => setPassword(e.target.value)} label="Password" />
+              
+             <button type="submit" disabled={!enable} className="h-[30px] bg-brand rounded-md font-semibold text-white text-sm disabled:opacity-70" >Log In</button>
+              <div className="flex items-center ">
+                <div className="h-px bg-gray-300 flex-1 "/>
+                  <span className="px-4 text-[13px] text-gray-500 font-semibold ">OR</span>
+                <div className="h-px bg-gray-300 flex-1 "/>
+                
+              </div>
+              <a href="#" className="flex justify-center items-center gap-x-2 text-sm font-semibold text-facebook ">
+                  <AiFillFacebook size={18} />
+                    Log in with Facebook
+                </a>
+           </form>    
+         </div>
+          
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,13 @@ module.exports = {
   theme: {
     
     extend: {
+      colors:{
+        brand:'#0095f6',
+        facebook: '#385185'
+      },
       backgroundImage: {
         'hero-pattern': "url(https://static.cdninstagram.com/images/instagram/xig/homepage/phones/home-phones.png?__makehaste_cache_breaker=HOgRclNOosk)",
-        'ss-pattern': "url(https://www.instagram.com/images/instagram/xig/homepage/screenshots/screenshot1.png?__d=www)",
-
+       
     },
     
   },
